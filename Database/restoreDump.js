01dfe826ff70d96fa5045319fdb3e6375bef59dd
Ntobeko Mvubu <ntobekoj7@gmail.com>
2025-10-27 23:58:20 +0200

    added some logic into the restoreDump function such as a helper function that will fetch the relevant data from the api and created an enum with all the apiLinks i will need

--- Database/restoreDump.js
+++ Database/restoreDump.js
@@ -1,5 +1,34 @@
-export function restoreDump(){
-   console.log("Hello") 
-};
+import dotenv from "dotenv";
+dotenv.config();
+export async function restoreDump() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        `Bearer ${process.env.API_KEY}`,
+    },
+  };
+
+   console.log(await fetchByLink(ApiLink.MovieList, options))
+
+}
+
+async function fetchByLink(link ,options){
+
+  try {
+    const response = await fetch(link, options)
+    const data = await response.json();
+    return data;
+  }
+  catch (err){
+    console.error("FectchByLink... ", err)
+  }
+}
+
+const ApiLink = {
+  GenreList: 'https://api.themoviedb.org/3/genre/movie/list?language=en',
+  MovieList: 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+}
 
 restoreDump();
