bee10bb4f9eae5dacb35919dc2af6713ba72b217
Ntobeko Mvubu <ntobekoj7@gmail.com>
2025-11-30 21:30:12 +0200

    Added new graphQl file for genre and added the relevant type and then added the resolver for the query that handles retrieving all the types and added another query  that fetchs the genre by the ID and the reolver for thar

---
+++ Backend/GQL_CinemaCentral/resolver/genre.js
@@ -0,0 +1,33 @@
+import query from "../../Common/db/db.js";
+export default {
+    Query: {
+        genreQuery: async () => {
+            const res = await query(`
+                select * from "Genre"
+            `);
+            return {
+                ok: true,
+                body: {
+                    statusCode: 200,
+                    genres: res.rows
+                }
+            }
+        },
+        genreFilterId: async (_, event) => {
+            const id = event.params.id;
+
+            const res = await query(`
+                SELECT * FROM "Genre" g 
+                    where g."id" = $1
+            `, [id]);
+            return {
+                ok: true,
+                body: {
+                    statusCode: 200,
+                    genres: res.rows
+                }
+            }
+
+        }
+    }
+}
