6e1ebac67ae941abbb3a5793ffb7dfcc942eb5de
Ntobeko Mvubu <ntobekoj7@gmail.com>
2026-01-08 23:22:26 +0200

    Feature/init frontend (#6)
    * created new Frontend folder and created the React app and connected the frontend to the backend
    
    * fixed bug on ApolloProvider and added http link as ApolloProvider needed a httplink

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import TopMainBar from './Components/TopMainBar/TopMainBar.jsx'
+function App() {
+  return (
+    <>
+      <TopMainBar />
+    </>
+  )
+}
+
+export default App
