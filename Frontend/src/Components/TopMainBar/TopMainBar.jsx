9ac58548041680e30c91a1933021cee0177496c0
Ntobeko Mvubu <ntobekoj7@gmail.com>
2026-01-24 19:13:06 +0200

    Feature/top main bar (#7)
    * added topMainBar
    
    * changed layout of top main bar and changed the styling and also added some responsiveness

---
+++ Frontend/src/Components/TopMainBar/TopMainBar.jsx
@@ -0,0 +1,50 @@
+import SvgIcon from '@mui/icons-material/TheatersOutlined';
+import TheatersOutlinedIcon from '@mui/icons-material/TheatersOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import './TopMainBar.css'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+function TopMainBar(){
+
+    console.log("TopMainBar component rendered");
+    return(
+        <nav className='top-main-bar'>
+            <section className='title-genre-section'>
+                <div className="main-title-section">
+                    <SvgIcon component={TheatersOutlinedIcon} fontSize='large' color='error'/>
+                    <h2>CineCentral</h2>  
+                </div>
+                <section className='genre-section'>
+                    <ul className='genre-list'>
+                        <li className='genre-list-item'>Action</li>
+                        <li className='genre-list-item'>Comedy</li>
+                        <li className='genre-list-item'>Drama</li>
+                        <li className='genre-list-item'>Sci-Fi</li>
+                        <li className='genre-list-item'>Romance</li>
+                    </ul>
+                </section>
+            </section>
+            <section className='search-section'>
+                <Autocomplete 
+                className='movie-search-input' 
+                options={['The Chosen']}
+                renderInput={(params)=> 
+                <TextField {...params}
+                    placeholder="Search movies..."  
+                    InputProps={{
+                        ...params.InputProps,
+                        startAdornment: (
+                            <SvgIcon component={SearchIcon}/>
+                        )
+                    }}
+                    /> 
+                }
+                />
+            </section>
+        </nav>
+    )
+    
+}
+
+export default TopMainBar
